6f0311215ddb91b31a62dfada748416cb36626aa
mthigpen <thigpenmm@gmail.com>
2021-04-24 10:47:19 -0500

    updates to views

--- CLC_Project/CLC-Project/resources/views/users.blade.php
+++ CLC_Project/CLC-Project/resources/views/users.blade.php
@@ -1,25 +1,91 @@
 <html>
-<body style="background-color:powderblue;">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta charset="ISO-8859-1">
-<link rel="stylesheet" href="../style.css">
-<title>Edit Product</title>
+<title>CLC Project: CST-256
+</title>
+<style>
+.content{
+background-image:linear-gradient(to right,#B7B7B7,#EAEAEA);
+height:600px;
+}
+<a href="#" class="myButton">dark grey</a>
+
+Button {
+	box-shadow:inset 0px 1px 3px 0px #91b8b3;
+	background:linear-gradient(to bottom, #768d87 5%, #6c7c7c 100%);
+	background-color:#768d87;
+	border-radius:5px;
+	border:1px solid #566963;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:11px 23px;
+	text-decoration:none;
+	text-shadow:0px -1px 0px #2b665e;
+}
+Button:hover {
+	background:linear-gradient(to bottom, #6c7c7c 5%, #768d87 100%);
+	background-color:#6c7c7c;
+}
+Button:active {
+	position:relative;
+	top:1px;
+}
+</style>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
-<h1 style="text-align:center;">User Registration</h1>
-<form action="accountRegistration" method="POST" style="text-align:center;">
-	<label for="name">Name:</label>
-	<input type="text" name="name" placeholder="Enter name" required/><br><br>
-	<label for="email">Email Address:</label>
-	<input type="email" name="email" placeholder="Enter email" required/><br><br>
-	<label for="age">Age:</label>
-	<input type="number" name="age" min="18" max="100" required/><br><br>
-	<label for="username">Username:</label>
-	<input type="text" name="username" placeholder="Enter username" required/> <br><br>
-	<label for="password">Password:</label>
-	<input type="password" name="password" placeholder="Enter password" required/> <br><br>
-	<button type="submit">Register Account!</button><br>
+<header>
+<nav class="navbar navbar-expand-lg navbar-light" style="background:#668B8B">
+<a class="navbar-brand" href="/"><h3>Job Search</h3></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+<div class="navbar-nav">
+@endif
+<</div>
+<div class="navbar-nav ml-auto">
+@if(Session::get('user'))
+<a class="nav-item nav-link" href="#">Welcome, {{Session::get('user')}}</a>
+<a class="nav-item nav-lhttp://marketplace.eclipse.org/marketplace-client-intro?mpc_install=4008412ink" href="/logout">Logout</a>
+@else
+<a class="nav-item nav-link active" href="/loginProcess">Login</a>
+<a class="nav-item nav-link active" href="/accountRegistration">Register</a>
+@endif
+</div>
+</div>
+</nav>
+</header>
+<div class="content d-flex justify-content-center">
+<div class="col-sm-8">
+<h3>User Registration</h3>
+<form action="accountRegistration" method="post" return="false">
+<div class="form-group">
+<label>Name
+<input type="text" name="name"class="form-control" placeholder="Enter Name" required>
+</div>
+<div class="form-group">
+<label>Email
+<input type="text" name="email" class="form-control" placeholder="Enter Email" required>
+</div>
+<div class="form-group">
+<label>Age
+<input type="age" name="age" class="form-control" placeholder="Enter Age" min="18" max="100" required>
+</div>
+<div class="form-group">
+<label>Username
+<input type="username" name="username"  class="form-control" placeholder="Enter Username" required>
+</div>
+<div class="form-group">
+<label>Password</label>
+<input type="password" name="password" class="form-control" placeholder="Enter Password" required>
+</div>
+<button class="Button" type="submit" style="background-color:#668B8B">Submit</button>
 </form>
-</body>
-</html>
+</div>
